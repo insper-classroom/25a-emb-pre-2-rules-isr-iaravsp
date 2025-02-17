161c57dbdc137314548be970a66f17d1b66f0d78
iara.vsp <iaravsp@al.insper.edu.br>
2025-02-17 13:38:24 -0300

    tentariva_isr_practice

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -5,26 +5,11 @@
 const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
-// ERRO: addon IsrVarVolatile
-int btn_flag;
+volatile int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-
-    // ERRO: addon IsrPrintf
-    printf("btn pressed \n");
-
-    // ERRO: addon IsrNoLoop
-    while (!pio_get(BTN_PIN_R)) {
-      // ERRO: addon IsrNoDelay
-      sleep_ms(1);
-    }
-
-    // ERRO: addon IsrPrintf
-    printf("btn released \n");
-
-    // ERRO: addon IsrNoDelay
-    sleep_ms(1);
+    
     btn_flag = 1;
   }
 }
@@ -43,6 +28,12 @@ int main() {
     if (btn_flag) {
       capture_flag = 1;
       btn_flag = 0;
+      printf("btn pressed \n");
+      while (!pio_get(BTN_PIN_R)) {
+        sleep_ms(1);
+      }
+      printf("btn released \n");
+      sleep_ms(1);
     }
 
     if (capture_flag) {
